b4725d77f4a63fc08e0601b0a4ead196584dd3a9
kongkong-std <zikang.qin95@gmail.com>
2024-06-03 09:12:39 -0500

    first commit

---
+++ include/featool_main.h
@@ -0,0 +1,9 @@
+#ifndef FEATOOL_MAIN_H_
+#define FEATOOL_MAIN_H_
+
+// hearder
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include "../include/featool_main.h"
+
+int main(int argc, char ** argv)
+{
+    printf("==== test FEATool ====\n");
+
+    return 0;
+}
